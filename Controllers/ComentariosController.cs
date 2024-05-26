7a975d0c1ce518aace95e2108f1b501ff1876c79
anna clara <anna.santos@byteon.com.br>
2024-05-26 15:34:56 -0300

    Finalizando comentarios

---
+++ Controllers/ComentariosController.cs
@@ -0,0 +1,62 @@
+﻿using BLOGAPI.Model.Data;
+using BLOGAPI.src.Contracts.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BLOGAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ComentariosController : ControllerBase
+    {
+        private readonly IComentariosService _comentarios;
+        public ComentariosController(IComentariosService comentarios)
+        {
+            _comentarios = comentarios;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] Comentarios comentarios)
+        {
+            try
+            {
+                return Ok(await _comentarios.Create(comentarios));
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("List/{Id}")]
+        public async Task<ActionResult> List(int Id)
+        {
+
+            try
+            {
+                return Ok(await _comentarios.List(Id));
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult> Delete(int Id)
+        {
+
+            try
+            {
+                return Ok(await _comentarios.Delete(Id));
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
+
--- src/Service/ComentariosService.cs
+++ src/Service/ComentariosService.cs
@@ -1,6 +1,30 @@
-﻿namespace BLOGAPI.src.Service
+﻿using BLOGAPI.Model.Data;
+using BLOGAPI.src.Contracts.Repository;
+using BLOGAPI.src.Contracts.Service;
+
+namespace BLOGAPI.src.Service
 {
-    public class Class
+    public class ComentariosService : IComentariosService
     {
+        private readonly IComentariosRepository _repository;
+        public ComentariosService(IComentariosRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Comentarios> Create(Comentarios comentarios)
+        {
+            return await _repository.Create(comentarios);
+        }
+
+        public async Task<bool> Delete(int Id)
+        {
+            return await _repository.Delete(Id);
+        }
+
+        public async Task<List<Comentarios>> List(int Id)
+        {
+            return await _repository.List(Id);
+        }
     }
 }
